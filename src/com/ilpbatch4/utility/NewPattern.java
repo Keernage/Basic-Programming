bf286fd52a38188d960437a3a6f6d69b54708e6f
RepairBoy <vijinrobinson14@gmail.com>
2024-06-04 14:51:20 +0530

    java file to print a right angled triangle using *

--- src/com/ilpbatch4/utility/NewPattern.java
+++ src/com/ilpbatch4/utility/NewPattern.java
@@ -3,8 +3,18 @@ package com.ilpbatch4.utility;
 public class NewPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int index = 5;
+		printPattern(index);
 
+	}
+
+	private static void printPattern(int index) {
+		for (int i = 0; i < index; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
 	}
 
 }
